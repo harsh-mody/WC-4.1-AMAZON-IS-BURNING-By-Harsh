2542d6e656647ef5a0ecdaa3dbaee2f6a17bced1
harshmody30401 <56110469+harshmody30401@users.noreply.github.com>
2020-01-08 15:08:14 +0530

    Delete WC 4.1 AMAZON IS BURNING By Harsh.py

--- WC 4.1 AMAZON IS BURNING By Harsh.py
+++
@@ -1,6 +0,0 @@
-array = list(input("Please Enter A List : "))
-arrayCount = []
-for i in range(1, 21):
-    if array.__contains__(str(i)):
-        arrayCount.append((array.count(str(i)), i))
-print(arrayCount)
